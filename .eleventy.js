258979e93a71f3369c65fba9735a24c52b0b1e65
zelfroster <sumitkumarsoni123@gmail.com>
2023-05-02 23:08:14 +0530

    fix: fixed index.html scss issue

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyVitePlugin);
-  eleventyConfig.addPassthroughCopy("./src/styles/");
+  eleventyConfig.addPassthroughCopy("./src/styles/*.scss");
   eleventyConfig.addPassthroughCopy("./src/assets/");
 
   return {
